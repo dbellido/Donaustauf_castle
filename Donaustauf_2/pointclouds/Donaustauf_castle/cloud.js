69ffeb3505157fbb34df7c56c853874713cb09b9
dbellido <becadiego@gmail.com>
2016-06-17 13:11:24 +0200

    ten cuidao

---
+++ Donaustauf_2/pointclouds/Donaustauf_castle/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 93999790,
+    "boundingBox": {
+        "lx": -194.62875,
+        "ly": -225.127828125,
+        "lz": -52.18752734375,
+        "ux": 805.6215781249999,
+        "uy": 775.1225,
+        "uz": 948.0628007812498
+    },
+    "tightBoundingBox": {
+        "lx": 65.37125009738665,
+        "ly": 369.87217187500007,
+        "lz": 325.81247265625,
+        "ux": 454.91771875000009,
+        "uy": 820.5580625,
+        "uz": 429.4838125
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 8.662422180175782,
+    "scale": 0.01,
+    "hierarchyStepSize": 5
+}
