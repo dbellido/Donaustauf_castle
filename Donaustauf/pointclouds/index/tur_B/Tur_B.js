6f5445ae8b29c8761f6aeb6b8a5e90b51a2c3d9b
dbellido <becadiego@gmail.com>
2016-06-17 14:02:13 +0200

    point_size

---
+++ Donaustauf/pointclouds/index/tur_B/Tur_B.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 9425403,
+    "boundingBox": {
+        "lx": 222.58003125,
+        "ly": 639.513,
+        "lz": 393.4645625,
+        "ux": 276.5551875,
+        "uy": 693.48815625,
+        "uz": 447.43971875
+    },
+    "tightBoundingBox": {
+        "lx": 222.58003126349377,
+        "ly": 639.5130000069813,
+        "lz": 393.4645625,
+        "ux": 276.5551874865062,
+        "uy": 667.4381874930187,
+        "uz": 410.74871875
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.46743857860565188,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
