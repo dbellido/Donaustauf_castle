3c9da9ccddd9fd480a6585685c9513ec1031e530
dbellido <becadiego@gmail.com>
2016-06-21 10:40:03 +0200

    wall_4

---
+++ Donaustauf/pointclouds/index/wall_4/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 13571878,
+    "boundingBox": {
+        "lx": 229.39215625,
+        "ly": 542.612125,
+        "lz": 411.825875,
+        "ux": 270.035875,
+        "uy": 583.2558437499999,
+        "uz": 452.46959374999997
+    },
+    "tightBoundingBox": {
+        "lx": 229.39215625000004,
+        "ly": 542.612125,
+        "lz": 411.825875,
+        "ux": 270.035875,
+        "uy": 568.2381875,
+        "uz": 429.4838125
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.3519849181175232,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
