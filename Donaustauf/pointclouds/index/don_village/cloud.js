0863ad1828af1834e1b1b7c0a8695b2ff10ab802
dbellido <becadiego@gmail.com>
2016-06-21 17:31:52 +0200

    don_village_PC

---
+++ Donaustauf/pointclouds/index/don_village/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 28849598,
+    "boundingBox": {
+        "lx": -194.62875,
+        "ly": -225.127828125,
+        "lz": -52.18752734375,
+        "ux": 256.057140625,
+        "uy": 225.5580625,
+        "uz": 398.49836328125
+    },
+    "tightBoundingBox": {
+        "lx": 65.37125009738665,
+        "ly": 369.87217187500007,
+        "lz": 325.81247265625,
+        "ux": 454.91771875000009,
+        "uy": 820.5580625,
+        "uz": 398.85585937499999
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 3.9030542373657228,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
