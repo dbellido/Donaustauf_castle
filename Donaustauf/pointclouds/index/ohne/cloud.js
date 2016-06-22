b6591e688ee802568c335e2cbb7ac077f5cf1683
dbellido <becadiego@gmail.com>
2016-06-22 14:50:24 +0200

    new PCs

---
+++ Donaustauf/pointclouds/index/ohne/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 8329107,
+    "boundingBox": {
+        "lx": 205.185,
+        "ly": 508.7875,
+        "lz": 365.925,
+        "ux": 471.5199999999999,
+        "uy": 775.1225,
+        "uz": 632.26
+    },
+    "tightBoundingBox": {
+        "lx": 205.18525,
+        "ly": 508.78925,
+        "lz": 365.9255,
+        "ux": 380.02250000000006,
+        "uy": 775.12175,
+        "uz": 426.064
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 2.3065288066864015,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
---
+++ Donaustauf/pointclouds/index/veg/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 12037068,
+    "boundingBox": {
+        "lx": 205.13,
+        "ly": 508.815,
+        "lz": 366.1725,
+        "ux": 471.43749999999997,
+        "uy": 775.1225,
+        "uz": 632.48
+    },
+    "tightBoundingBox": {
+        "lx": 205.13225,
+        "ly": 508.81675,
+        "lz": 366.17325,
+        "ux": 379.997,
+        "uy": 775.12175,
+        "uz": 427.4285
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 2.306290626525879,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
